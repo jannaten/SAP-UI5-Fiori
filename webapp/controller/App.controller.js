d1441413fd7a4b031ef75a45fc06d062811e7985
Jannaten Nayem <jannaten@eduix.it>
2024-03-29 15:38:36 +0200

    translation added

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,8 +3,9 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
   ],
-  (Controller, MessageToast, JSONModel) => {
+  (Controller, MessageToast, JSONModel, ResourceModel) => {
     "use strict";
     return Controller.extend("ui5.walkthrough.controller.App", {
       onInit() {
@@ -15,9 +16,18 @@ sap.ui.define(
         };
         const oModel = new JSONModel(oData);
         this.getView().setModel(oModel);
+        var i18nModel = new ResourceModel({
+          bundleName: "ui5.walkthrough.i18n.i18n",
+          supportedLocales: [""],
+          fallbackLocale: "",
+        });
+        this.getView().setModel(i18nModel, "i18n");
       },
       onShowHello() {
-        MessageToast.show("Hello World");
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        MessageToast.show(sMsg);
       },
     });
   }
