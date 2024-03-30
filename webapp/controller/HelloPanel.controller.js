9b9b0eeb68bf8dabaaf118535eaddcaf48bdb77a
Jannaten Nayem <jannaten@eduix.it>
2024-03-30 15:06:40 +0200

    on close method added

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -22,6 +22,9 @@ sap.ui.define(
         });
         this.pDialog.then((oDialog) => oDialog.open());
       },
+      onCloseDialog() {
+        this.byId("helloDialog").close();
+      },
     });
   }
 );
