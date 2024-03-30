fdb899ecfce9ebe71d4511cf759366e68055883c
Jannaten Nayem <jannaten@eduix.it>
2024-03-30 12:14:21 +0200

    components added

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,32 +1,18 @@
 sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-  ],
-  (Controller, MessageToast, JSONModel, ResourceModel) => {
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  (Controller, MessageToast) => {
     "use strict";
+
     return Controller.extend("ui5.walkthrough.controller.App", {
-      onInit() {
-        const oData = {
-          recipient: {
-            name: "World",
-          },
-        };
-        const oModel = new JSONModel(oData);
-        this.getView().setModel(oModel);
-        var i18nModel = new ResourceModel({
-          bundleName: "ui5.walkthrough.i18n.i18n",
-          supportedLocales: [""],
-          fallbackLocale: "",
-        });
-        this.getView().setModel(i18nModel, "i18n");
-      },
       onShowHello() {
-        var oBundle = this.getView().getModel("i18n").getResourceBundle();
-        var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        // read msg from i18n model
+        const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        const sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+        // show message
         MessageToast.show(sMsg);
       },
     });
